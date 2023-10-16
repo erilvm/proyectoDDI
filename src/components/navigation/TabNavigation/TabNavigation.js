921723bf38d612ad4bbc21cff35f0367744b1664
erilvm <erika.nemesio2002@gmail.com>
2023-10-15 18:13:16 -0600

    estilo de screenCuenta

--- src/components/navigation/TabNavigation/TabNavigation.js
+++ src/components/navigation/TabNavigation/TabNavigation.js
@@ -1,54 +1,83 @@
-import { View } from 'react-native'
+import React from 'react';
+import { View, TouchableOpacity, Alert } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { styles } from './TabNavigation.styles';
 import CustomTabBarButton from './CustomTabBarButton';
 import AwesomeIcon from 'react-native-vector-icons/FontAwesome';
-
 import StackNavigation from '../../navigation/StackNavigation/StackNavigation';
 import StackFavoritos from '../../navigation/StackNavigation/StackFavoritos';
-
 import AccountScreen from '../../../screen/AccountScreen';
-
+import { useAuth } from '../../../hooks/useAuth';
 
 const Tab = createBottomTabNavigator();
 
 export default function TabNavigation() {
-  return (
+  const { logout } = useAuth();
 
+  // Función para manejar el evento de cerrar sesión
+  const handleLogout = () => {
+  // Mostrar una alerta para confirmar el cierre de sesión
+
+    Alert.alert(
+      'Cerrar Sesión',
+      '¿Estás seguro de que deseas cerrar sesión?',
+      [
+        {
+          text: 'Cancelar',
+          style: 'cancel',
+        },
+        {
+          text: 'Aceptar',
+          onPress: () => {
+            // Si se presiona "Aceptar", llamar a la función de cerrar sesión esta se encuentra en useAuth
+            logout();
+          },
+        },
+      ],
+    );
+  };
+
+  return (
     <Tab.Navigator
-    initialRouteName="Inicio" // Inicializa en HomeScreen 
-    screenOptions={({ route }) => ({
-    tabBarIcon: (routeStatus) => setIcon(route, routeStatus),
-    tabBarStyle: {
-      backgroundColor: '#0D0D0D', // Cambia el color de fondo de la barra superior
-    },
-  })}
->
+      initialRouteName="Inicio"
+      screenOptions={({ route }) => ({
+        tabBarIcon: (routeStatus) => setIcon(route, routeStatus),
+        tabBarStyle: {
+          backgroundColor: '#0D0D0D',
+        },
+      })}
+    >
+      <Tab.Screen
+        name="Cuenta"
+        component={AccountScreen}
+        options={{
+          tabBarLabel: 'Mi perfil',
+          title: '',
+          headerShown: true,
+          headerTransparent: true,
+          headerRight: () => (
+            <TouchableOpacity style={{ marginRight: 10 }} onPress={handleLogout}>
+              <AwesomeIcon name="sign-out" size={24} color="white" />
+            </TouchableOpacity>
+          ),
+        }}
+      />
 
       <Tab.Screen
-  name="Cuenta"
-  component={AccountScreen}
-  options={{
-    title: 'Mi Cuenta',
-    headerShown: false
-    
-  }}
-/>
-<Tab.Screen
-      name="Inicio"
-      component={StackNavigation}
-      options={{
-        title: 'Inicio',
-        headerShown: false,
-        tabBarButton: () => (
-          //icono de boton en la pantalla de home
-          <CustomTabBarButton iconSource={require('../../../assets/home.png')} />
-        ),
-      }}
-    />
+        name="Inicio"
+        component={StackNavigation}
+        options={{
+          title: 'Inicio',
+          headerShown: false,
+          tabBarButton: () => (
+            <CustomTabBarButton iconSource={require('../../../assets/home.png')} />
+          ),
+        }}
+      />
+
       <Tab.Screen
         name="StackFavoritos"
-        component={StackFavoritos} 
+        component={StackFavoritos}
         options={{
           title: 'Favoritos',
           headerShown: false,
@@ -60,20 +89,21 @@ export default function TabNavigation() {
 
 const setIcon = (route, routeStatus) => {
   let iconName = '';
-  let color = '#F0F2EB'; //se determina el color de los botones de la barra superior
-  if (routeStatus.focused) {
-    color = '#A7CB54'; //se determina el color de cuando el boton esta activo
-    }
-//se determina los iconos para cada boton
-    if(route.name === "Inicio"){
-      iconName="home"
-    }
-    if(route.name === "Cuenta"){
-      iconName="user"
-    }
-    if(route.name === "StackFavoritos"){
-      iconName="heart"
-    }
-return <AwesomeIcon name={iconName} color={color} style={styles.icon}/>
+  let color = '#F0F2EB';
 
-}
+  if (routeStatus.focused) {
+    color = '#A7CB54';
+  }
+
+  if (route.name === 'Inicio') {
+    iconName = 'home';
+  }
+  if (route.name === 'Cuenta') {
+    iconName = 'user';
+  }
+  if (route.name === 'StackFavoritos') {
+    iconName = 'heart';
+  }
+
+  return <AwesomeIcon name={iconName} color={color} style={styles.icon} />;
+};
--- src/screen/AccountScreen.js
+++ src/screen/AccountScreen.js
@@ -1,74 +1,101 @@
-import React, { useContext } from 'react';
-import { View, Text, StyleSheet, Button, ScrollView, Image } from 'react-native';
-import { Avatar, ActivityIndicator, MD2Colors } from 'react-native-paper';
-import { useAuth} from '../hooks/useAuth';
-import { Appbar } from 'react-native-paper';
-import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
+import React from 'react';
+import { View, Text, StyleSheet, ScrollView, ImageBackground, Image } from 'react-native';
+import { useAuth } from '../hooks/useAuth';
 
+const fondoArriba = require('../assets/fondoPerfil.jpg');
+const fondoAbajo = require('../assets/info.jpeg');
+const personaImage = require('../assets/persona3.png');
 
 export default function AccountScreen() {
-  const { user, logout } = useAuth();
-  console.log('Datos del usuario', user)
+  const { user } = useAuth();
 
   return (
-    
     <View style={styles.container}>
-    <Appbar.Header style={{ backgroundColor: '#5CAD4A' }}>
-    <Appbar.Content title="Mi cuenta" alignItems='center' />
-    <Appbar.Action icon={() => <FontAwesomeIcon name="sign-out" size={24} />} onPress={logout} />
-    </Appbar.Header>
-
-
-      <View style={styles.header}>
-
-        <View style={{ ...styles.avatar, width: 140, height: 140 }}>
-          <Image style={{ flex: 1, width: null, height: null }} source={require('../assets/persona3.png')} resizeMode="cover" />
-        </View>
-
-      </View>
-          <ScrollView>
-          <View style={styles.mainContainer}>
-            <Text style={styles.username}>Usuario: {user.username}</Text>
-            <Text style={styles.email}>Email: {user.email}</Text>
-      </View>
-      </ScrollView>
-
-      <View style={styles.footer}>
-
+      <View style={styles.topHalf}>
+        <ImageBackground source={fondoArriba} style={styles.backgroundImage}>
+          <View style={styles.header}>
+            <View style={styles.avatarContainer}>
+              <Image style={styles.avatar} source={personaImage} resizeMode="cover" />
+            </View>
+          </View>
+        </ImageBackground>
       </View>
 
+      <View style={styles.bottomHalf}>
+        <ImageBackground source={fondoAbajo} style={styles.bottomBackground}>
+          <View style={styles.userInfoContainer}>
+            <ScrollView>
+              <View style={styles.userInfo}>
+                <Text style={styles.username}>Usuario: {user.username}</Text>
+                <Text style={styles.email}>Email: {user.email}</Text>
+              </View>
+            </ScrollView>
+          </View>
+        </ImageBackground>
+      </View>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
   },
-  mainContainer: {
-        padding: 20,
-        borderRadius: 10,
-        margin: 20,
-      },
+  topHalf: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  bottomHalf: {
+    flex: 1,
+  },
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'flex-end',
+  },
+  bottomBackground: {
+    flex: 1,
+    resizeMode: 'cover',
+  },
   header: {
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
-    marginTop: 40,
+    paddingTop: 30,
+    paddingBottom: 20,
+    marginTop: -50,
+  },
+  avatarContainer: {
+    width: 170, //tamaño del avatar
+    height: 170,
+    borderRadius: 60,
+    overflow: 'hidden',
+    margin: 87  // Ajuste para subir el avatar más arriba
+  },
+  avatar: {
+    flex: 1,
+    width: null,
+    height: null,
+  },
+  userInfoContainer: {
+    flex: 1,
+    //backgroundColor: 'rgba(255, 255, 255, 0.7)', //fondo transparente
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    padding: 20,
+  },
+  userInfo: {
+    padding: 20,
   },
   username: {
     fontSize: 20,
     fontWeight: 'bold',
     marginBottom: 10,
-    textAlign: "center"
+    textAlign: 'center',
+    color: 'white'
   },
   email: {
     fontSize: 16,
-  },
-  footer: {
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  avatar: {
-    top: 30,
+    color: 'white'
   },
 });
